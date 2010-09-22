b52523586c073ff5e70d151f5819dc9c2e3f751c
Jeff Smith <jeff@jeffsmith.me>
2010-09-22 09:35:11 -0300

    Working example with text and button.

---
+++ src/com/example/androidexample/AndroidExample.java
@@ -0,0 +1,13 @@
+package com.example.androidexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidExample extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
