cbd38694384ec5bad880c3ada62008a34e144a20
Jeff Smith <jeff@jeffsmith.me>
2010-09-22 10:43:13 -0300

    Modifying toast.

--- src/com/example/androidexample/AndroidExample.java
+++ src/com/example/androidexample/AndroidExample.java
@@ -2,6 +2,14 @@ package com.example.androidexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class AndroidExample extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +17,21 @@ public class AndroidExample extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		final Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		        // Perform action on clicks
+		        // Toast.makeText(AndroidExample.this, "Beep Bop", Toast.LENGTH_LONG).show();
+				LayoutInflater inflater = getLayoutInflater();
+				View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_layout_root));
+
+				Toast toast = new Toast(getApplicationContext());
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.setDuration(Toast.LENGTH_SHORT);
+				toast.setView(layout);
+				toast.show();
+		    }
+		});
 	}
 }
